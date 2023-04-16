54167d420f2ad40b1538d7e7bb7b51a914af4742
Raich94 <raichxxx@mail.ru>
2023-04-16 21:50:25 +0300

    feat: tasks complete dream, hanoi, tramsform, recuesive, extended, dns

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!(arr instanceof Array)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  };
+
+  const result = [];
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === '--discard-next'){
+      i++;
+    } else if(arr[i] === '--double-prev'){
+      arr[i - 2] !== '--discard-next' && result.push(arr[i - 1]);
+    } else if(arr[i] === '--double-next'){
+      result.push(arr[i + 1]);
+    } else if(arr[i] === '--discard-prev'){
+      arr[i - 2] !== '--discard-next' && result.pop();
+    } else {
+      result.push(arr[i])
+    }
+  }
+
+  return result.filter(a => a !== undefined);
 }
 
 module.exports = {
